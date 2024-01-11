d2d57973259a4f03bdd8280100df529e7dca13f9
shuaiwu1108 <wslio@hotmail.com>
2024-01-11 17:24:12 +0800

    Create V20240111__menu_insert.sql

---
+++ src/main/resources/db/migration/V20240111__menu_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `menu` (`id`, `parent_id`, `code`, `name`, `create_time`, `update_time`) VALUES (11, NULL, 'webrtc', 'webrtc', '2024-01-09 15:14:06', '2024-01-09 15:14:09');
+INSERT INTO `menu` (`id`, `parent_id`, `code`, `name`, `create_time`, `update_time`) VALUES (12, NULL, 'vehicle_ocr', '车牌识别', '2024-01-11 09:15:13', '2024-01-11 09:15:13');
+INSERT INTO `menu` (`id`, `parent_id`, `code`, `name`, `create_time`, `update_time`) VALUES (13, NULL, 'face_ocr', '人脸识别', '2024-01-11 17:15:34', '2024-01-11 17:15:36');
+INSERT INTO `menu` (`id`, `parent_id`, `code`, `name`, `create_time`, `update_time`) VALUES (14, NULL, 'card_ocr', '证件识别', '2024-01-11 17:15:58', '2024-01-11 17:16:01');
